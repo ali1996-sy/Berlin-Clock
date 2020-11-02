d79e8d5f699ec8d1bfbaf55ba6f32fe6397f8c37
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-11-02 23:30:01 +0100

    creat function display_simple_hours

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,8 +3,18 @@
 
 class BerlinClock
 {
+    private $simpleHours;
 public function __construct()
 {
+    $this->simpleHours=array("1stHour","2ndHour","3rdHour","4thHour");
 
+}
+public function display_simple_hours($hours): String{
+    $text="{";
+    for($i=0;$i<$hours;$i++){
+        $text+=$this->simpleHours[i];
+    }
+    $text+="}";
+    return $text;
 }
 }
