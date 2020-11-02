fcb9e1ae17b39d5c7a0496182d806774133ee166
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-11-03 00:59:39 +0100

    display_bloc5_hours

--- BerlinClock.php
+++ BerlinClock.php
@@ -4,9 +4,11 @@
 class BerlinClock
 {
     private $simpleHours;
+    private $bloc5_hours;
 public function __construct()
 {
     $this->simpleHours=array("1stHour","2ndHour","3rdHour","4thHour");
+    $this->bloc5_hours=array("5h","10h","15h","20h");
 
 }
 public function display_simple_hours($hours): String{
@@ -17,4 +19,12 @@ public function display_simple_hours($hours): String{
     $text+="}";
     return $text;
 }
+    public function display_bloc5_hours($nbbloc5): String{
+        $text="{";
+        for($i=0;$i<$nbbloc5;$i++){
+            $text+=$this->bloc5_hours[i]+",";
+        }
+        $text+="}";
+        return $text;
+    }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -14,19 +14,19 @@ class BerlinClockTest extends TestCase
     }
     public function test_simple_hours_1min_return_1st(){
        $actual=$this->berlinClock->display_simple_hours(1);
-       $this->assertEquals("1st",$actual);
+       $this->assertEquals("{1st,}",$actual);
     }
     public function test_simple_hours_2min_return_1st_2nd(){
         $actual=$this->berlinClock->display_simple_hours(2);
-        $this->assertEquals("1st,2nd,",$actual);
+        $this->assertEquals("{1st,2nd,}",$actual);
     }
     public function test_simple_hours_2min_return_1st_2nd_3rd(){
         $actual=$this->berlinClock->display_simple_hours(3);
-        $this->assertEquals("1st,2nd,3rd,",$actual);
+        $this->assertEquals("{1st,2nd,3rd,}",$actual);
     }
     public function test_simple_hours_2min_return_1st_2nd_3rd_4th(){
         $actual=$this->berlinClock->display_simple_hours(4);
-        $this->assertEquals("1st,2nd,3rd,",$actual);
+        $this->assertEquals("{1st,2nd,3rd,4th,}",$actual);
     }
 
 }
