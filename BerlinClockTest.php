4043af59e7fd2521ac4c23ad02e9725d86097b1d
Abdulrazzak <abdulrssak@gmail.com>
2020-11-03 21:34:15 +0100

    testMethode5Min cas3 20min

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -39,6 +39,10 @@ class BerlinClockTest extends TestCase
         $actual = $this->display_5_by_5_minutes(15);
         $this->assertEquals("[ON][ON[ON][OFF][OFF][OFF][OFF][OFF][OFF][OFF][OFF]"  , $actual);
     }
+    public  function test_display_givenMinutes_5_shouldReturn_ON_4_OFF_7(){
+        $actual = $this->display_5_by_5_minutes(20);
+        $this->assertEquals("[ON][ON[ON][ON][OFF][OFF][OFF][OFF][OFF][OFF][OFF]"  , $actual);
+    }
 
 
 }
