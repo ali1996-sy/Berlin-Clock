b311163aa3f1e612c444b534740c196bce4fd1de
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-11-03 16:39:48 +0100

    refactoring display_NBseconds_pair_or_Impair

--- BerlinClock.php
+++ BerlinClock.php
@@ -32,9 +32,9 @@ public function display_simple_hours($hours): String{
         return $text;
     }
     public function display_NBseconds_pair_or_Impair($seconds): String{
-        $text="{";
-        if($seconds%2==0)$text=$text."pair";
-        else$text=$text."impair";
+        $text="{round :";
+        if($seconds%2==0)$text=$text."ON";
+        else$text=$text."OFF";
         $text=$text."}";
         return $text;
     }
